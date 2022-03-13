f69db9f05decddec46e2a33edb42800a9ea8cd1d
mordenius <mordenius@gmail.com>
2022-03-13 12:08:32 +0200

    refactor(main) firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import './views/register_view.dart';
 import './views/login_view.dart';
+import './firebase_options.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,7 +21,31 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginView(),
+      home: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                final emailVerified =
+                    FirebaseAuth.instance.currentUser?.emailVerified ?? false;
+                if (emailVerified) {
+                  print('Email verified');
+                } else {
+                  print('You need to verify your email first');
+                }
+
+                return Scaffold(
+                  appBar: AppBar(
+                    title: const Text("Hello"),
+                  ),
+                  body: const LoginView(),
+                );
+              default:
+                return const Text('Loading...');
+            }
+          }),
     );
   }
 }
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import '../firebase_options.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
@@ -30,96 +28,71 @@ class _RegisterViewState extends State<RegisterView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Hello"),
-      ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
+    return Column(
+      children: [
+        TextField(
+          controller: _email,
+          keyboardType: TextInputType.emailAddress,
+          decoration: const InputDecoration(hintText: "Enter your email"),
         ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.waiting:
-              return const Text("Loading...");
-            case ConnectionState.done:
-              return Center(
-                  child: Column(
-                children: [
-                  TextField(
-                    controller: _email,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration:
-                        const InputDecoration(hintText: "Enter your email"),
-                  ),
-                  TextField(
-                    controller: _pswd,
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    decoration:
-                        const InputDecoration(hintText: "Enter your peassword"),
-                  ),
-                  TextButton(
-                    child: const Text("Register"),
-                    onPressed: () async {
-                      final email = _email.text;
-                      final pswd = _pswd.text;
-                      try {
-                        final userCredentials = await FirebaseAuth.instance
-                            .createUserWithEmailAndPassword(
-                                email: email, password: pswd);
-                        print(userCredentials);
-                      } on FirebaseAuthException catch (e) {
-                        if (e.code == "weak-password") {
-                          return showDialog(
-                              context: context,
-                              builder: (ctx) => AlertDialog(
-                                    title: const Text(
-                                        'Use more complexity password'),
-                                    content: Text('${e.message}'),
-                                  ));
-                        }
+        TextField(
+          controller: _pswd,
+          obscureText: true,
+          enableSuggestions: false,
+          autocorrect: false,
+          decoration: const InputDecoration(hintText: "Enter your peassword"),
+        ),
+        TextButton(
+          child: const Text("Register"),
+          onPressed: () async {
+            final email = _email.text;
+            final pswd = _pswd.text;
+            try {
+              final userCredentials = await FirebaseAuth.instance
+                  .createUserWithEmailAndPassword(email: email, password: pswd);
+              print(userCredentials);
+            } on FirebaseAuthException catch (e) {
+              if (e.code == "weak-password") {
+                return showDialog(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                          title: const Text('Use more complexity password'),
+                          content: Text('${e.message}'),
+                        ));
+              }
 
-                        if (e.code == "email-already-in-use") {
-                          return showDialog(
-                              context: context,
-                              builder: (ctx) => AlertDialog(
-                                    title: const Text('Email already in use'),
-                                    content: Text('${e.message}'),
-                                  ));
-                        }
+              if (e.code == "email-already-in-use") {
+                return showDialog(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                          title: const Text('Email already in use'),
+                          content: Text('${e.message}'),
+                        ));
+              }
 
-                        if (e.code == "invalid-email") {
-                          return showDialog(
-                              context: context,
-                              builder: (ctx) => AlertDialog(
-                                    title: const Text('Email is invalid'),
-                                    content: Text('${e.message}'),
-                                  ));
-                        }
+              if (e.code == "invalid-email") {
+                return showDialog(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                          title: const Text('Email is invalid'),
+                          content: Text('${e.message}'),
+                        ));
+              }
 
-                        showDialog(
-                            context: context,
-                            builder: (ctx) => AlertDialog(
-                                  title:
-                                      const Text(' Ops! Registration Failed'),
-                                  content: Text('${e.message}'),
-                                ));
-                      } catch (error) {
-                        print('Something went wrong');
-                        print(error);
-                      }
-                    },
-                    style: const ButtonStyle(backgroundColor: null),
-                  )
-                ],
-              ));
-            default:
-              return const Text("Something went wrong");
-          }
-        },
-      ),
+              showDialog(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                        title: const Text(' Ops! Registration Failed'),
+                        content: Text('${e.message}'),
+                      ));
+            } catch (error) {
+              print('Something went wrong');
+              print(error);
+            }
+          },
+          style: const ButtonStyle(backgroundColor: null),
+        )
+      ],
     );
   }
 }
